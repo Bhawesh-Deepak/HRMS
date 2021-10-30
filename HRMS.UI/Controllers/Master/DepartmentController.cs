83b2eecae6f32c9b59274e404cba92ecaf3ff0a7
Bhawesh Deepak <Bhawesh Deepak@DESKTOP-KB2SSTK>
2021-10-29 23:16:18 -0700

    Response

--- HRMS.UI/Controllers/Master/DepartmentController.cs
+++ HRMS.UI/Controllers/Master/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using HRMS.Core.Entities.Master;
 using HRMS.Core.Helpers.CommonCRUDHelper;
 using HRMS.Services.Repository.GenericRepository;
+using HRMS.UI.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -22,17 +23,28 @@ namespace HRMS.UI.Controllers.Master
             return View();
         }
 
-        [HttpGet]
-        public async Task<IActionResult> CreateDepartment()
+        [HttpPost]
+        public async Task<IActionResult> CreateDepartment(Department model)
         {
-            Department model = new Department();
-            model.Name = "XTXXX";
-            model.Code = "Dept01";
-            model.Description = "Description";
+            var response = await _IDepartmentRepository.CreateEntity(CrudHelper.CreateHelper<Department>(model));
 
-            var createModel = CrudHelper.CreateHelper<Department>(model);
+            return Json(ResponseMessageHelper.GetResponseMessage(response.ResponseStatus,
+                nameof(Department), nameof(CreateDepartment)));
 
-            var response = await _IDepartmentRepository.CreateEntity(createModel    );
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDepartmentList()
+        {
+            var dbResponse = await _IDepartmentRepository.GetAllEntities(x => x.IsActive && !x.IsDeleted);
+
+            var response = ResponseMessageHelper.GetResponseMessage(dbResponse.ResponseStatus, nameof(Department), nameof(GetDepartmentList));
+
+            if (response.isSuccess)
+            {
+
+            }
+
             return Json("");
 
         }
