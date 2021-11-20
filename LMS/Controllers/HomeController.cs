28cff575ffe65c87e60cea1033ea948acdb91887
sunil kumar sudhansu <sunil@klbs.in>
2021-11-20 14:05:10 +0530

    export data changes

--- LMS/Controllers/HomeController.cs
+++ LMS/Controllers/HomeController.cs
@@ -22,12 +22,14 @@ namespace LMS.Controllers
     public class HomeController : Controller
     {
         private readonly IGenericRepository<CustomerDetail, int> _ICustomerDetailRepository;
+        private readonly IGenericRepository<LeadType, int> _ILeadTypeRepository;
         private readonly IGenericRepository<CustomerLeadDetail, int> _ICustomerLeadRepository;
         private readonly ILogger<HomeController> _ILogger;
-        public HomeController(IGenericRepository<CustomerDetail, int> iCustomerDetailRepository,
+        public HomeController(IGenericRepository<CustomerDetail, int> iCustomerDetailRepository, IGenericRepository<LeadType, int> iLeadTypeRepository,
               IGenericRepository<CustomerLeadDetail, int> customerLeadRepo, ILogger<HomeController> logger)
         {
             _ICustomerDetailRepository = iCustomerDetailRepository;
+            _ILeadTypeRepository = iLeadTypeRepository;
             _ICustomerLeadRepository = customerLeadRepo;
             _ILogger = logger;
         }
@@ -79,10 +81,10 @@ namespace LMS.Controllers
         }
         public async Task<IActionResult> ExportCustomerLead(int? LeadTypeId)
         {
-            List<CustomerDetail> responseDetails = await GetCustomerDetailLeadTypeWise(LeadTypeId);
+            List<CompleteLeadsDetailVM> responseDetails = await GetCustomerDetailLeadTypeWise(LeadTypeId);
 
             DataTable dt = new DataTable("LeadDetails");
-            dt.Columns.AddRange(new DataColumn[7] {
+            dt.Columns.AddRange(new DataColumn[8] {
                     new DataColumn("LeadName"),
                     new DataColumn("Location"),
                     new DataColumn("Phone"),
@@ -90,11 +92,12 @@ namespace LMS.Controllers
                     new DataColumn("Description/Project"),
                     new DataColumn("Special Remarks"),
                     new DataColumn("AssignDate"),
+                     new DataColumn("Status"),
             });
 
             foreach (var data in responseDetails)
             {
-                dt.Rows.Add(data.LeadName, data.Location, data.Phone, data.Email, data.Description_Project, data.SpecialRemarks, data.AssignDate.ToString("dd/MM/yyyy"));
+                dt.Rows.Add(data.LeadName, data.Location, data.Phone, data.Email, data.Description_Project, data.SpecialRemarks, data.AssignDate.ToString("dd/MM/yyyy"), data.LeadTypeName);
             }
 
             using XLWorkbook wb = new XLWorkbook();
@@ -104,28 +107,30 @@ namespace LMS.Controllers
             return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                 "CustomerLeadDetails.xlsx");
         }
-        private async Task<List<CustomerDetail>> GetCustomerDetailLeadTypeWise(int? LeadTypeId)
+        private async Task<List<CompleteLeadsDetailVM>> GetCustomerDetailLeadTypeWise(int? LeadTypeId)
         {
             var CustomerDetailList = await _ICustomerDetailRepository.GetAllEntities(x => x.IsActive && !x.IsDeleted);
             var CustomerLeadLIst = await _ICustomerLeadRepository.GetAllEntities(x => x.IsActive && !x.IsDeleted);
+            var LeadTypeLIst = await _ILeadTypeRepository.GetAllEntities(x => x.IsActive && !x.IsDeleted);
             if (LeadTypeId == null)
             {
                 var responseDetails = (from CDList in CustomerDetailList.Entities
                                        join CLList in CustomerLeadLIst.Entities
                                        on CDList.Id equals CLList.CustomerId
+                                       join ltype in LeadTypeLIst.Entities on CLList.LeadType equals ltype.Id into leadtype
+                                       from subpet in leadtype.DefaultIfEmpty()
                                        where CLList.EmpId == Convert.ToInt32(HttpContext.Session.GetString("empId"))
-                                       select new CustomerDetail
+                                       select new CompleteLeadsDetailVM
                                        {
-                                           Id = CDList.Id,
+                                           CustomerId = CDList.Id,
                                            LeadName = CDList.LeadName,
                                            Location = CDList.Location,
                                            Phone = CDList.Phone,
                                            Email = CDList.Email,
                                            Description_Project = CDList.Description_Project,
                                            AssignDate = CDList.AssignDate,
-                                           SpecialRemarks = CDList.SpecialRemarks
-
-
+                                           SpecialRemarks = CDList.SpecialRemarks,
+                                           LeadTypeName = subpet?.Name ?? String.Empty
                                        }).ToList();
                 return responseDetails;
             }
@@ -134,19 +139,20 @@ namespace LMS.Controllers
                 var responseDetails = (from CDList in CustomerDetailList.Entities
                                        join CLList in CustomerLeadLIst.Entities
                                        on CDList.Id equals CLList.CustomerId
+                                       join ltype in LeadTypeLIst.Entities on CLList.LeadType equals ltype.Id into leadtype
+                                       from subpet in leadtype.DefaultIfEmpty()
                                        where CLList.EmpId == Convert.ToInt32(HttpContext.Session.GetString("empId")) && CLList.LeadType == LeadTypeId
-                                       select new CustomerDetail
+                                       select new CompleteLeadsDetailVM
                                        {
-                                           Id = CDList.Id,
+                                           CustomerId = CDList.Id,
                                            LeadName = CDList.LeadName,
                                            Location = CDList.Location,
                                            Phone = CDList.Phone,
                                            Email = CDList.Email,
                                            Description_Project = CDList.Description_Project,
                                            AssignDate = CDList.AssignDate,
-                                           SpecialRemarks = CDList.SpecialRemarks
-
-
+                                           SpecialRemarks = CDList.SpecialRemarks,
+                                           LeadTypeName = subpet?.Name ?? String.Empty
                                        }).ToList();
                 return responseDetails;
             }
